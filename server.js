a7db80249d33668c4ca72dd3f34e0c8cbff364ed
Omar Khalil <omareldagestany@outlook.com>
2022-06-20 17:40:54 -0400

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-
-require('dotenv').config;
+require("dotenv").config;
 
 const app = express();
 const port = process.env.port || 5000;
@@ -11,17 +10,20 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Copyres:Soridl846@cluster0.ohmco.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://Copyres:Soridl846@cluster0.ohmco.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
   const collection = client.db("gallerydb").collection("devices");
   // perform actions on the collection object
   client.close();
 });
 
-
 app.listen(port, () => {
-    console.log(`Server is running on Port: ${port}`);
+  console.log(`Server is running on Port: ${port}`);
 });
