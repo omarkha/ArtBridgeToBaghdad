8ff2d735fb12919bb9584aa82144a046004c45b3
Omar Khalil <omareldagestany@outlook.com>
2022-06-20 17:58:18 -0400

    third try

--- server.js
+++ server.js
@@ -2,27 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-require("dotenv").config;
-
 const app = express();
 const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://Copyres:Soridl846@cluster0.ohmco.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+app.use(express.static(`${__dirname}/client/build`));
+app.get("/*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
 });
-client.connect((err) => {
-  const collection = client.db("gallerydb").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+const mongoose = require("mongoose");
+require("dotenv").config(); // Add this line
+
+let dbUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGODB_URI
+    : "mongodb://127.0.0.1:27017/gallerydb";
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+mongoose.set("debug", true);
+const db = mongoose.connection;
+
+module.exports = db;
 
 app.listen(process.env.port || 5000, () => {
   console.log(`Server is running on Port: ${port}`);
