7ce27dd2d66756714a8595a8191179dc70156333
Omar Khalil <omareldagestany@outlook.com>
2022-06-21 22:16:34 -0400

    message

--- client/src/store/actions/TodoActions.js
+++
@@ -1,6 +0,0 @@
-import { ADD_TODO } from "../types";
-
-export const AddTodo = (todo) => ({
-  type: ADD_TODO,
-  payload: todo,
-});
--- client/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import TodoReducer from "./reducers/TodoReducer";
-
-const store = createStore(TodoReducer, composeWithDevTools());
-
-export default store;
