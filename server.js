8e9156973cd7c597632300f43f89c03195ed08d4
Omar Khalil <omareldagestany@outlook.com>
2022-07-01 03:48:53 -0400

    asdasd

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose
     console.error("Connection error", e.message);
   });
 mongoose.set("debug", true);
-const db = mongoose.connection;
+mongoose.connection;
 
 // Routes
 app.use("/api/paintings", require("./routes/paintings"));
