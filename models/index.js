ad178cc0c16bd42b2773c3b1914f57f9f736a3ad
Omar Khalil <omareldagestany@outlook.com>
2022-07-01 10:48:53 -0400

    trying

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 const PaintingSchema = require("./painting.model");
+const SubscriberSchema = require("./subscriber.model");
 
-const Painting = mongoose.model("Book", PaintingSchema);
+const Painting = mongoose.model("Painting", PaintingSchema);
+
+const Subscriber = mongoose.model("Subscriber", SubscriberSchema);
 
 module.exports = {
   Painting,
+  Subscriber,
 };
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ mongoose.connection;
 
 // Routes
 app.use("/api/paintings", require("./routes/paintings"));
-app.use("/api/subscriber", require("./routes/subscribers"));
+app.use("/api/subscribers", require("./routes/subscribers"));
 //
 app.get("/*", (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
