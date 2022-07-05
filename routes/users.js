5874429b2a1b1a401deeb4c5ae0e80dd495c525c
Omar Khalil <omareldagestany@outlook.com>
2022-07-04 21:51:46 -0400

    asdasd

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/userControllers");
-const passport = require("passport");
-const passportConfig = require("../passport");
 
 router.post("/", userControllers.postUser);
 router.post("/login", userControllers.loginUser);
