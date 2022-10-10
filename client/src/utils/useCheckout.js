a4a9f42eec5eb0b6ea04e8cabbd1a3466454b0cd
Omar Khalil <omareldagestany@outlook.com>
2022-10-10 07:55:35 -0400

    nicely

--- client/src/utils/useCheckout.js
+++ client/src/utils/useCheckout.js
@@ -3,12 +3,17 @@ import toast from "react-hot-toast";
 import { redirect } from "react-router-dom";
 import { useShoppingCart } from "use-shopping-cart";
 
+const uri =
+  process.env.NODE_ENV === "production"
+    ? "https://artbridgetobaghdad.herokuapp.com"
+    : "http://localhost:5000";
+
 export default function useCheckout() {
   const { redirectToCheckout, cartDetails } = useShoppingCart();
 
   async function handleCheckout() {
     const session = await axios
-      .post("/api/checkout-sessions", cartDetails)
+      .post(`${uri}/api/checkout-sessions`, cartDetails)
       .then((res) => {
         console.log(res.data);
         return res.data;
